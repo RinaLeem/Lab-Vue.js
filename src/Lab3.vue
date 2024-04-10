887a970ace07319a389a6e90ce3f3f8994829cd0
Екатерина Балашова <rinaleem@mail.ru>
2024-04-10 16:58:59 +0400

    Fixed lab3

--- src/Lab3.vue
+++ src/Lab3.vue
@@ -1,121 +1,103 @@
 <script setup>
-import {ref, computed} from 'vue';
+import {ref, onMounted} from 'vue';
 
-const history = ref ([]);
-const taskText = ref('');
-const taskAmound = ref('');
-let id = 0;
+const myNumber = ref('');
+const numberType = ref('trivia');
+const fact = ref(null);
 
-function addToList() {
-    if (taskText.value != '' && taskAmound.value != '') {
-        history.value.push({ text: taskText.value, amound: taskAmound.value, id: id++ });
-        taskText.value = '';
-        taskAmound.value = 0;
+const loading = ref(false);
+const show = ref(false);
+
+async function handleSearch() {
+    loading.value = true;
+    try {
+        const response = await fetch(`http://numbersapi.com/${myNumber.value}/${numberType.value}?json`);
+        fact.value = await response.json();
+    } catch (e) {
+        console.error('Error', e)
+    } finally {
+        loading.value = false;
     }
 }
 
-const incomeBalance = computed(() => {
-    let totalIncome = 0;
-    for (let task of history.value) {
-        if (task.amound > 0) {
-            totalIncome += task.amound;
-        }
+// async function handleSearch() {
+//     loading.value = true;
+//     try {
+//         const {data} = await getInfo( );
+//         fact.value = data;
+//     } catch (e) {
+//         console.error('Error', e)
+//     } finally {
+//         loading.value = false;
+//     }
+// }
+// function getInfo( ) {
+//     return fetch(`http://numbersapi.com/${myNumber.value}/${numberType.value}?json`, {
+//         method: 'GET',
+//     })
+//         .then(response => response.json())
+// }
+
+onMounted(() => {
+    handleSearch();
+})
+
+const getMessage = () => {
+    if (numberType.value === 'year') {
+        return 'скучный год';
+    } else {
+        return 'скучное число';
     }
-    return totalIncome;
-});
+};
 
-const outcomeBalance = computed(() => {
-    let totalIncome = 0;
-    for (let task of history.value) {
-        if (task.amound < 0) {
-            totalIncome += task.amound;
-        }
-    }
-    return totalIncome;
-});
-
-const totalBalance = computed(() => {
-    let totalIncome = 0;
-    for (let task of history.value) {
-        totalIncome += task.amound;
-    }
-    return totalIncome;
-});
-
-
+function findFact(){
+    show.value = true;
+    handleSearch();
+}
 
 </script>
 
 <template>
     <main>
-        <div class = "container">
-            <h1>Учет расходов</h1> <br>
-            <div v-if="history.length" class="balance">
-                <label name="balance">Баланс: </label>
-                <span name="balance" >{{totalBalance}}</span><br>
-                <label name="income">Доходы: </label>
-                <span name="income">{{incomeBalance}}</span><br>
-                <label name="expenses">Расходы: </label>
-                <span name="expenses">{{outcomeBalance}}</span><br>
-                
-            </div>
-            <template v-else>
-                <h4>Вы не совершали финансовых операций</h4>
-            </template><br>
-            
-            
-            <form @submit.prevent="addToList">
-                <input type="text" v-model.trim="taskText" placeholder="Введите название"/> <br>
-                <input  type="number" v-model.number="taskAmound" placeholder="Введите сумму"/> <br>
-                <input type="button" @click="addToList" value="Добавить" class="button"><br><br>
-            </form>  
+        <div class="container">
+            <h1>Случайные факты о числах</h1><br>        
 
-            <div v-for="item of history" :key="item.id" class="list"><hr>
-                <label :for="item.id"> {{ item.text }} <span class="task_amound" :class = 'item.amound>0 ? "plus":"minus"'>{{ item.amound }}</span></label> 
-            </div>
-               
+            <form @submit.prevent="handleSearch">
+                <input type="number" v-model.number="myNumber" placeholder="Введите число"/><br>
+                <input type="text" list="types" v-model="numberType" placeholder="Вид факта" ><br>
+                <datalist id="types">
+                    <option value="math"></option>
+                    <option value="year"></option>
+                    <option value="trivia"></option> 
+                </datalist><br>
+                <input type="button" @click="findFact" value="Искать" class="button"><br><br>
+            </form> 
 
+            <div v-if="show">
+                <div v-if="loading">Загрузка...</div>
+                <div v-else-if="fact.found">{{ fact.text }} </div>
+                <div v-else>{{myNumber}} - {{getMessage()}} </div>
+            </div>
+            
         </div>
     </main>
 </template>
 
 <style scoped>
 .container{
-    width: 400px;
+    width: 500px;
     border: 4px;
     border-style:groove;
     padding: 20px;
-    
-}
-.balance{
-    border-style:groove;
-    padding: 5px;
-    
 }
 input{
-    width: 350px;
-    background-color:rgb(219, 234, 240);
-
+    width: 150px;
+    background-color:rgb(232, 238, 241);
 }
 .button{
     height: 40px;
     margin-top: 10px;
     background-color:rgb(198, 217, 224);
-}
-.list{
-    width: 350px;
-    position: relative;
-}
-.task_amound{
-    position: absolute;
-    right: 0px;
-}
-
-.plus{
-    color: green;
-}
-.minus{
-    color:red;
 }
 
 </style>
